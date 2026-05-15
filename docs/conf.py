f0b46b5fcf107a304884dc13361d321429277e4b
baldurk <baldurk@baldurk.org>
2026-05-15 16:20:20 +0100

    Include needed jquery dependency for rtd theme
    * We ship this locally as python 3.6 installs an older sphinx, which doesn't
      seem to have this available.

--- docs/conf.py
+++ docs/conf.py
@@ -58,7 +58,7 @@ sys.path.insert(0, os.path.abspath('sphinx_exts'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx_paramlinks']
+extensions = ['sphinx.ext.autodoc', 'sphinx_paramlinks', 'sphinxcontrib_jquery']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
