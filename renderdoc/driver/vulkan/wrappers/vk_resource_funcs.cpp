7b69f79bad5dffb6f3fba8a1ba1264a200797850
Emma Anholt <anholt@igalia.com>
2026-09-10 16:17:30 -0700

    Look for a descriptor-heap memory type for transfers, too.
    anv and radv limit to a subset of memory type bits based on either of
    these flags being present.

--- renderdoc/driver/vulkan/wrappers/vk_resource_funcs.cpp
+++ renderdoc/driver/vulkan/wrappers/vk_resource_funcs.cpp
@@ -461,12 +461,23 @@ bool WrappedVulkan::Serialise_vkAllocateMemory(SerialiserType &ser, VkDevice dev
         ObjDisp(device)->GetBufferMemoryRequirements(Unwrap(device), buf, &mrq);
 
         // Can't create a memory-spanning buffer for this allocation.
-        // For descriptor buffers try again if that is enabled as those memory types are sometimes unique.
-        if((((1 << AllocateInfo.memoryTypeIndex) & mrq.memoryTypeBits) == 0) && DescriptorBuffers())
+        // For descriptor buffers/heaps try again if that is enabled as those memory types are sometimes unique.
+        if((((1 << AllocateInfo.memoryTypeIndex) & mrq.memoryTypeBits) == 0) &&
+           (DescriptorBuffers() || DescriptorHeap()))
         {
           ObjDisp(device)->DestroyBuffer(Unwrap(device), buf, NULL);
 
-          bufInfo.usage |= VK_BUFFER_USAGE_RESOURCE_DESCRIPTOR_BUFFER_BIT_EXT;
+          if(DescriptorBuffers())
+            bufInfo.usage |= VK_BUFFER_USAGE_RESOURCE_DESCRIPTOR_BUFFER_BIT_EXT;
+
+          VkBufferUsageFlags2CreateInfo bufInfo2 = {
+              VK_STRUCTURE_TYPE_BUFFER_USAGE_FLAGS_2_CREATE_INFO};
+          if(DescriptorHeap())
+          {
+            bufInfo2.usage = bufInfo.usage | VK_BUFFER_USAGE_2_DESCRIPTOR_HEAP_BIT_EXT;
+            bufInfo2.pNext = bufInfo.pNext;
+            bufInfo.pNext = &bufInfo2;
+          }
 
           ret = ObjDisp(device)->CreateBuffer(Unwrap(device), &bufInfo, NULL, &buf);
           RDCASSERTEQUAL(ret, VK_SUCCESS);
@@ -737,10 +748,20 @@ VkResult WrappedVulkan::vkAllocateMemory(VkDevice device, const VkMemoryAllocate
         wholeMemBuf = VK_NULL_HANDLE;
 
         // can't create a memory-spanning buffer for this allocation. Try again with descriptor
-        // buffers if that is enabled as those memory types are sometimes unique.
-        if(DescriptorBuffers())
+        // buffers/heaps if that is enabled as those memory types are sometimes unique.
+        if(DescriptorBuffers() || DescriptorHeap())
         {
-          bufInfo.usage |= VK_BUFFER_USAGE_RESOURCE_DESCRIPTOR_BUFFER_BIT_EXT;
+          if(DescriptorBuffers())
+            bufInfo.usage |= VK_BUFFER_USAGE_RESOURCE_DESCRIPTOR_BUFFER_BIT_EXT;
+
+          VkBufferUsageFlags2CreateInfo bufInfo2 = {
+              VK_STRUCTURE_TYPE_BUFFER_USAGE_FLAGS_2_CREATE_INFO};
+          if(DescriptorHeap())
+          {
+            bufInfo2.usage = bufInfo.usage | VK_BUFFER_USAGE_2_DESCRIPTOR_HEAP_BIT_EXT;
+            bufInfo2.pNext = bufInfo.pNext;
+            bufInfo.pNext = &bufInfo2;
+          }
 
           ret = ObjDisp(device)->CreateBuffer(Unwrap(device), &bufInfo, NULL, &wholeMemBuf);
           RDCASSERTEQUAL(ret, VK_SUCCESS);
